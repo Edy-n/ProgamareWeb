57172691a153545387405bdc05513db27a896052
Petrov Eduard <lsd2404@gmail.com>
2026-03-21 16:08:54 +0200

    feat: created a card and liked it to the app

---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+function NumeComponenta() {
+	return (
+		<section>
+			<h1>INFO CARD</h1>
+			<ul style={{ listStyleType: 'none' }}>
+				<li>Nume: Petrov Eduard</li>
+				<li>Varsta: 20</li>
+				<li>Student: CALC Anul 2</li>
+
+			</ul>
+		</section>
+	);
+}
+export default NumeComponenta;
