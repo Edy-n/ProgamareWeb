38fcd6a2bb53a74dd8e7d0a8c55bfa6a569c905e
Petrov Eduard <lsd2404@gmail.com>
2026-03-25 15:33:11 +0200

    ex6 lab 4
    Exercitiu bonus buton decrement si reset .

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import Card from './Card.jsx'; 
-import {useState} from 'react';	
+import Card from './Card.jsx';
+import { useState } from 'react';
 
 function App() {
-   const projects = [
-      { title: "Proiect 1", description: "Pagina personala" },
-      { title: "Proiect 2", description: "Calculator buget" },
-      { title: "Proiect 3", description: "Dashboard React" },
-   ];
-	const [count , setCount] = useState(0);
+	const projects = [
+		{ title: "Proiect 1", description: "Pagina personala" },
+		{ title: "Proiect 2", description: "Calculator buget" },
+		{ title: "Proiect 3", description: "Dashboard React" },
+	];
+	const [count, setCount] = useState(0);
 
-   return (
-      <div>
-         <h1>Dashboard</h1>
-         <p>Petrov Eduard</p>
-         
-         {projects.map(function (item, index) {
-            return (
-               <Card 
-                  key={index} 
-                  title={item.title} 
-                  description={item.description} 
-               />
-            );
-         })}
+	return (
+		<div>
+			<h1>Dashboard</h1>
+			<p>Petrov Eduard</p>
+
+			{projects.map(function (item, index) {
+				return (
+					<Card
+						key={index}
+						title={item.title}
+						description={item.description}
+					/>
+				);
+			})}
 			<p>Ai apasat pe {count} ori</p>
-			<button onClick = {() =>  setCount(count + 1)}>Apasa-ma</button>
-      </div>
-   );
+			<button onClick={() => setCount(count + 1)}>Increment </button>
+			<button onClick={() => setCount(count - 1)}>Decrement</button>
+			<button onClick={() => setCount(count - count)}>reset</button>
+		</div>
+	);
 }
 
 export default App;
